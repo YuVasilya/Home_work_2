3e53392df265033fce6ff80d826401eb89e4d484
Vasilya <lady_drakula_94@mail.ru>
2022-09-26 00:42:10 +0500

    Программа работает, но нужно ввести команду bool или придумать что-то другое

---
+++ Ex002/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+Console.WriteLine("Find third number");
+int N = new Random().Next(100, 1000);
+Console.Write("Загадано число = ");
+Console.WriteLine(N);
+if (N >= 100)
+{
+    int a = N / 100;
+    int c = (N - a * 100) % 10;
+    Console.WriteLine($"Thitd number: {c}");
+}
+Console.WriteLine("Thitd number is not found");
