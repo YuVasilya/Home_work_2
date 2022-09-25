d00f1c92b2e0b9e350b0f948963519672df70bc3
Vasilya <lady_drakula_94@mail.ru>
2022-09-26 00:16:38 +0500

    Ex001. It Was easy

---
+++ Ex001/Program.cs
@@ -0,0 +1,8 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа
+Console.WriteLine("This Program find second number.");
+int N = new Random().Next(100, 1000);
+Console.Write("Загадано число = ");
+Console.WriteLine(N);
+int a = N/100;
+int b = (N - a*100)/10;
+Console.WriteLine($"Second number: {b}");
