738e54f92fd25ea35b7e683613f8943c1d932d8b
Vasilya <lady_drakula_94@mail.ru>
2022-09-26 12:43:40 +0500

    I did it!

---
+++ Ex003/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+Console.Clear();
+Console.WriteLine("Сегодня выходной?");
+int N = new Random().Next(1, 8);
+Console.WriteLine($"N:{N}");
+//bool holliday = false;
+if (N > 5)
+{
+    Console.WriteLine("Да");
+}
+else
+{
+    Console.WriteLine("Нет");
+}
+Console.WriteLine("End");    
