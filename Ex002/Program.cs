455fa21d8efaab2762f657a301e693de7d25ddc5
Vasilya <lady_drakula_94@mail.ru>
2022-09-26 13:50:16 +0500

    Ex002 работает

--- Ex002/Program.cs
+++ Ex002/Program.cs
@@ -3,10 +3,22 @@ Console.WriteLine("Find third number");
 int N = new Random().Next(100, 1000);
 Console.Write("Загадано число = ");
 Console.WriteLine(N);
-if (N >= 100)
+
+string NText = Convert.ToString(N);
+if (NText.Length>2)
+{
+Console.Write("third number = ");
+Console.WriteLine(NText[2]);
+}
+else
+{
+Console.WriteLine("Third numberis not found");
+}
+Console.WriteLine("End");
+/*if (N >= 100)
 {
     int a = N / 100;
     int c = (N - a * 100) % 10;
     Console.WriteLine($"Thitd number: {c}");
 }
-Console.WriteLine("Thitd number is not found");
+Console.WriteLine("Thitd number is not found");*/
